3fe19457f40a16b13bdafac06e84dac5c82fd40c
larswal03 <lars@wco.no>
2022-01-16 21:56:31 +0100

    oppdateringer

---
+++ Julekortgenerator/Models/ChristmasCard.cs
@@ -0,0 +1,20 @@
+﻿namespace Julekortgenerator.Models
+{
+    public class ChristmasCard
+    {
+        public string BackGroundColor { get; set; } = "rgb(255, 40, 0)";
+        public string BorderColor { get; set; } = "rgb(255, 40, 0)";
+        public string TextColor1 { get; set; } = "rgb(255, 255, 255)";
+        public string TextColor2 { get; set; } = "rgb(255, 255, 255)";
+        public string TextColor3 { get; set; } = "rgb(255, 255, 255)";
+        public string TextColor4 { get; set; } = "rgb(255, 255, 255)";
+        public string Header { get; set; } = "God jul!";
+        public string Greeting { get; set; } = "";
+        public string ReceiverName { get; set; } = "";
+        public string SenderName { get; set; } = "";
+        
+        public string[] Images { get; set; } = new string[] { "", "", "" };
+        
+        public string MyFont { get; set; } = "'Courier New', monospace";
+    }
+}
